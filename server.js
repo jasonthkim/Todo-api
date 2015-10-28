ef39088854f02e142322994aef41436a42a07f28
Jason Kim <jasonthkim@gmail.com>
2015-10-27 23:08:35 -0700

    Final static todos.

--- server.js
+++ server.js
@@ -14,19 +14,23 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=house
 app.get('/todos', function (req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
-
-	// if has property && completed === 'true'
-	// filteredTodos = _.where(filteredTodos, ?)
-	// else if has property && completed is 'false'
 
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	// q params need a min length function
+	// "Go to work on Saturday".indexOf('work')
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
 	}
 
 	res.json(filteredTodos);
