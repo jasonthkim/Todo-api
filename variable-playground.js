3ddc201f7e3d83384af869f4e852f0f9fae39ed1
Jason Kim <jasonthkim@gmail.com>
2015-10-27 22:55:02 -0700

    Add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+// 	name: 'Jason',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Jason',
+// 	// 	age: 33
+// 	// }
+// 	obj.age = 33;
+// }
+
+// updatePerson (person);
+// console.log(person);
+
+// Array Example
+var grades = [99, 12];
+
+function  addGrades (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
